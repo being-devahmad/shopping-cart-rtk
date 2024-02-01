3e5bf8ab869df960cc5053663a705a6e127a6996
being-devahmad <devahmad41@gmail.com>
2024-02-01 18:44:57 +0500

    second commit

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,47 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { remove } from "../Redux/CartSlice";
+import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = () => {
   const dispatch = useDispatch();
   const cartItems = useSelector((state) => state.cart);
+  console.log(cartItems);
+
+  const [quantities, setQuantities] = useState(
+    cartItems.reduce((acc, item) => ({ ...acc, [item.id]: 1 }), {})
+  );
 
   const handleRemove = (id) => {
     dispatch(remove(id));
   };
 
+  const decrement = (id) => {
+    if (quantities[id] > 1) {
+      setQuantities((prevQuantities) => ({
+        ...prevQuantities,
+        [id]: prevQuantities[id] - 1,
+      }));
+    }
+  };
+
+  const increment = (id) => {
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [id]: prevQuantities[id] + 1,
+    }));
+  };
+
+  const totalAmount = cartItems.reduce(
+    (acc, item) => acc + quantities[item.id] * item.price,
+    0
+  );
+
   return (
     <>
       <div className="container">
         <div className="row">
-          <div className="col-8 m-auto">
-            {cartItems.map((item) => (
-              <div
-                key={item.id}
-                className="border d-flex justify-content-around p-3 mb-3"
-              >
-                <img
-                  src={item.image}
-                  style={{ width: "50px", height: "50px" }}
-                />
-                <div>{item.title.slice(0, 20)}</div>
-                <div>${item.price}</div>
-                <div className="d-flex gap-2">
+          <div className="col-lg-8 m-auto">
+            {cartItems.map((item) => {
+              const { id, image, title, price } = item;
+              return (
+                <div
+                  key={id}
+                  className="border d-flex justify-content-around align-items-center p-3 mb-3"
+                >
+                  <img src={image} style={{ width: "50px", height: "50px" }} />
+                  <div>{title.slice(0, 20)}</div>
+                  <div>${price}</div>
+                  <div className="d-flex items-center align-items-center gap-2">
+                    <button
+                      className="btn btn-dark"
+                      onClick={() => decrement(id)}
+                    >
+                      -
+                    </button>
+                    <span>{quantities[id]}</span>
+                    <button
+                      className="btn btn-dark"
+                      onClick={() => increment(id)}
+                    >
+                      +
+                    </button>
+                  </div>
+
+                  <i
+                    class="bi bi-x-lg"
+                    style={{ cursor: "pointer" }}
+                    onClick={() => handleRemove(id)}
+                  ></i>
+                </div>
+              );
+            })}
+          </div>
+          <div className="col-lg-4">
+            <div className="border py-2 px-3 mb-3">
+              <div>
+                <h3>Order Summary</h3>
+                <div className="border d-flex">
+                  <h4 className="me-5">Total</h4>
+                  <h4 className="ps-5">${totalAmount}</h4>
+                </div>
+                <div>
+                  <NavLink to="/">
+                    <button className="btn btn-primary mt-5 w-100">
+                      Proceed to shopping
+                    </button>
+                  </NavLink>
+                </div>
+                <div>
                   <button
-                    className="btn btn-dark"
-                    // onClick={() => removeFromCart(item.id)}
-                    onClick={() => handleRemove(item.id)}
+                    className="btn btn-danger mt-5 w-100"
+                    // onClick={checkout}
                   >
-                    Delete
+                    Checkout
                   </button>
                 </div>
               </div>
-            ))}
+            </div>
           </div>
-          <NavLink to={"/"} className="btn btn-dark mt-3">
-            Continue shopping
-          </NavLink>
         </div>
       </div>
     </>
